710b007eed55ed9ba34d77e32c1acd2f3fe0c423
Nicolás Monzón <nmonzon.2022@gmail.com>
2023-04-26 06:57:07 -0300

    Some algorithms

---
+++ src/main/java/org/adt/core/adt/algorithms/StackAlgorithms.java
@@ -0,0 +1,47 @@
+package org.adt.core.adt.algorithms;
+
+import org.adt.core.adt.definition.IStack;
+import org.adt.core.adt.implementation.normal.Stack;
+
+public class StackAlgorithms {
+
+    public static void print(IStack stack) {
+        while (!stack.isEmpty()) {
+            System.out.println(stack.getTop());
+            stack.remove();
+        }
+    }
+
+    public static IStack copy(IStack stack) {
+        IStack copy = new Stack();
+        IStack aux = new Stack();
+        while (!stack.isEmpty()) {
+            aux.add(stack.getTop());
+            stack.remove();
+        }
+        while(!aux.isEmpty()) {
+            stack.add(aux.getTop());
+            copy.add(aux.getTop());
+            aux.remove();
+        }
+        return copy;
+    }
+
+    public static void revert(IStack stack) {
+        IStack aux = new Stack();
+        IStack aux2 = new Stack();
+        while (!stack.isEmpty()) {
+            aux.add(stack.getTop());
+            stack.remove();
+        }
+        while (!aux.isEmpty()) {
+            aux2.add(aux.getTop());
+            aux.remove();
+        }
+        while (!aux2.isEmpty()) {
+            stack.add(aux2.getTop());
+            aux2.remove();
+        }
+    }
+
+}
